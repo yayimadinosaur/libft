4958dc26dda474f7a53ee16c6188e380cf4e69b8
warren fung <wfung@e1z1r3p12.42.us.org>
2016-12-20 15:25:58 -0800

    fixed memmove; protected lstmap, but doesn't work yet

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/15 15:50:05 by wfung             #+#    #+#             */
-/*   Updated: 2016/12/15 21:12:44 by wfung            ###   ########.fr       */
+/*   Updated: 2016/12/20 08:16:50 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	t_list	*current;
 
 	current = lst;
-	while (current->next != NULL)
+	if (current == NULL)
+		return (NULL);
+	while (current != NULL)
 	{
 		if (!f(current))
 			return (NULL);
-		else
-			current = current->next;
+		f(current);
+		free((void*)current);
+		current = current->next;
 	}
 	return (current);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 14:55:44 by wfung             #+#    #+#             */
-/*   Updated: 2016/12/18 20:23:05 by wfung            ###   ########.fr       */
+/*   Updated: 2016/12/20 15:24:12 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,29 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t	i;
-	char	*dst_buff;
-	char	*src_buff;
+	size_t			i;
+	unsigned char	*src_buf;
+	unsigned char	*dst_buf;
 
 	i = 0;
-	dst_buff = (char*)dst;
-	src_buff = (char*)src;
-	while (i < len)
+	src_buf = (unsigned char*)src;
+	dst_buf = (unsigned char*)dst;
+	if (*src_buf >= *dst_buf || *src_buf >= (*dst_buf + len))
 	{
-		dst_buff[i] = src_buff[i];
-		i++;
+		while (len)
+		{
+			dst_buf[i] = src_buf[i];
+			i++;
+			len--;
+		}
 	}
-	return ((void*)dst_buff);
+	else
+		dst_buf = dst_buf + len - 1;
+		src_buf = src_buf + len - 1;
+		while (len)
+		{
+			*dst_buf-- = *src_buf--;
+			len--;
+		}
+	return ((void*)dst_buf);
 }
