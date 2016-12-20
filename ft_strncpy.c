7850f56bd83fd378ce252801edad3be7b3fd595d
warren fung <wfung@e1z2r6p1.42.us.org>
2016-12-19 16:38:54 -0800

    fixed strncpy

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 13:54:36 by wfung             #+#    #+#             */
-/*   Updated: 2016/12/19 15:46:56 by wfung            ###   ########.fr       */
+/*   Updated: 2016/12/19 16:37:50 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,23 @@
 
 char	*ft_strncpy(char *dst, const char *src, size_t len)
 {
-	unsigned int i;
+	unsigned int	i;
+	unsigned int	x;
 
 	i = 0;
-	while (i < len)
+	x = ft_strlen(src);
+	while (src[i] != '\0' && i < len)
 	{
-		if (src[i] != '\0')
-			dst[i] = src[i];
-		else if (src[i] == '\0')
-			dst[i] = '\0';
+		dst[i] = src[i];
 		i++;
+	}
+	if (src[i] == '\0')
+		dst[i] = '\0';
+	while (x < len)
+	{
+		dst[i] = '\0';
+		i++;
+		x++;
 	}
 	return (dst);
 }
