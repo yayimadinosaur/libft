c6b07d1e21094364d20b319077cf8c41e2e1f195
warren fung <wfung@e1z2r6p1.42.us.org>
2016-12-26 19:48:57 -0800

    fixedgit statusgit status took 1.5 weeks

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/05 16:53:09 by wfung             #+#    #+#             */
-/*   Updated: 2016/12/22 16:04:51 by wfung            ###   ########.fr       */
+/*   Updated: 2016/12/26 19:48:22 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,46 +18,22 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	i;
 	size_t	j;
 	size_t	k;
-	size_t	x;
+	size_t	p;
 
 	i = 0;
-	j = ft_strlen(dst);
-	k = ft_strlen(src);
-	x = size - j;
-	if (size < j + k + 1)
-	{
-		printf("size too small!\n'dst' = '%s'\n'src' = '%s'\n", dst, src);
-		printf("size = %zu\ndst len = '%zu'\nsrc len = '%zu'\n", size, j, k);
-		return (size + k + 1);
-	}
-	if (size - 1 == j)
-		return (size + k - 1);
-	while (dst[i] != '\0')
-	{
+	j = 0;
+	k = ft_strlen(dst);
+	p = ft_strlen(src);
+	while (i <= k && i < size && dst[i] != '\0')
 		i++;
-		size--;
-	}
-	while (src[i] != '\0' && (size - j - 1) > 0)
+	if (size <= k)
+		return (i + p);
+	while (src[j] != '\0' && j < size - k - 1)
 	{
-		dst[i] = src[i];
+		dst[i] = src[j];
 		i++;
-		size--;
+		j++;
 	}
 	dst[i] = '\0';
-	printf("inside ft_strlcat = '%s\n'", dst);
-	return (j + k);
+	return (k + p);
 }
-
-
-int		main(void)
-{
-	char	s1[] = "hello"; // len = 6;	dst;
-	char	s2[] = "1";		//	len = 1; src;
-	size_t	i;				// size = 2;
-
-	i = 6;
-	printf("Your ftn = '%zu'\n", ft_strlcat(s1, s2, i));
-	printf("Built In = '%zu'\n", strlcat(s1, s2, i));
-	return (0);
-}
-
