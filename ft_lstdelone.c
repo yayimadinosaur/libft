a12b789feb02b3623986f5feabea040a4b553b4d
warren fung <wfung@e1z2r6p1.42.us.org>
2016-12-30 16:46:41 -0800

    fixed lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/15 15:35:07 by wfung             #+#    #+#             */
-/*   Updated: 2016/12/20 16:58:21 by wfung            ###   ########.fr       */
+/*   Updated: 2016/12/30 15:51:39 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
-	t_list	*current;
-
-	current = *alst;
-	while (current != current->next)
-	{
-		del(current, current->content_size);
-		free(current);
-		current = NULL;
-		current = current->next;
-	}
+	if (!alst)
+		return ;
+	if (!*alst)
+		return ;
+	del((*alst)->content, (*alst)->content_size);
+	free(*alst);
+	*alst = NULL;
+	return ;
 }
