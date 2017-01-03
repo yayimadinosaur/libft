6c8208f05f411927ffeb9e48ec40810fa4467cd6
warren fung <wfung@e1z1r3p12.42.us.org>
2017-01-03 13:50:46 -0800

    fixed strmp_len

--- ft_strcmp_len.c
+++ ft_strcmp_len.c
@@ -6,7 +6,7 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/02 17:49:29 by wfung             #+#    #+#             */
-/*   Updated: 2017/01/02 17:57:12 by wfung            ###   ########.fr       */
+/*   Updated: 2017/01/03 13:49:47 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,10 @@ size_t	ft_strcmp_len(char *s1, char *s2)
 {
 	if (!s1 || !s2)
 		return (-1);
+	if (s1 > s2)
+		return (ft_strlen(s1) - ft_strlen(s2));
+	else if (s1 < s2)
+		return (ft_strlen(s2) - ft_strlen(s1));
 	else
-	{
-		if (s1 > s2)
-			return (ft_strlen(s1) - ft_strlen(s2));
-		else if (s1 < s2)
-			return (ft_strlen(s2) - ft_strlen(s1));
-		else if (s1 == s2)
-			return (0);
-	}
+		return (0);
 }
